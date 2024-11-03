0edf8e14087094db0a1c62b114738575b2d10422
prathoseraaj V <146630697+prathoseraaj0312@users.noreply.github.com>
2024-11-03 18:54:57 +0530

    feat: created a simple xml and css with a useState of js

---
+++ src/Content.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+const Content = () => {
+
+    //State the input
+    let [input,setInput] = useState("");
+
+        let handleButton = () => {
+        document.getElementsByClassName('button')
+    }
+
+  return (
+    <main>
+    <div className='container'>
+        <div id='textbox'></div>
+        <div className='column'>
+        <div ><button id='botton7' className='button' >7</button></div>  
+        <div ><button id='botton8' className='button' >8</button></div>        
+        <div><button id='botton9' className='button' >9</button></div>        
+        <div><button id='botton/' className='button' >/</button></div>        
+        </div>
+
+        <div className='column'>
+        <div><button id='botton4' onChange={() => handleButton(4)}>4</button></div>  
+        <div><button id='botton5'>5</button></div>        
+        <div><button id='botton6'>6</button></div>        
+        <div><button id='botton*'>*</button></div>        
+        </div>
+
+        <div className='column'>
+        <div><button id='botton1'>1</button></div>  
+        <div><button id='botton2'>2</button></div>        
+        <div><button id='botton3'>3</button></div>        
+        <div><button id='botton-'>-</button></div>        
+        </div>
+
+        <div className='column'>
+        <div><button>0</button></div>  
+        <div><button>.</button></div>        
+        <div><button>+</button></div>        
+        <div ><button id='equalto'>=</button></div>        
+        </div>
+    </div>
+    </main>
+  )
+}
+
+export default Content
