11b753f5278401a3a9be09fd69ef76fe459ba323
prathoseraaj V <146630697+prathoseraaj0312@users.noreply.github.com>
2024-11-03 19:07:06 +0530

    feat: added a onclick func and induced a setInput to see the equation

--- src/Content.js
+++ src/Content.js
@@ -2,8 +2,12 @@ import React, { useState } from 'react';
 
 const Content = () => {
 
-    //State the input
     let [input,setInput] = useState("");
+
+    let handlebutton = (value) => {
+        setInput(input + value) ;
+    }
+
 
     
   return (
@@ -11,24 +15,24 @@ const Content = () => {
     <div className='container'>
         <div id='textbox'></div>
         <div className='column'>
-        <div ><button id='botton7' className='button' >7</button></div>  
-        <div ><button id='botton8' className='button' >8</button></div>        
-        <div><button id='botton9' className='button' >9</button></div>        
-        <div><button id='botton/' className='button' >/</button></div>        
+        <div ><button id='botton7' className='button' onClick={() => handlebutton("7")}>7</button></div>  
+        <div ><button id='botton8' className='button'  onClick={() => handlebutton("8")} >8</button></div>        
+        <div><button id='botton9' className='button'  onClick={() => handlebutton("9")}>9</button></div>        
+        <div><button id='botton/' className='button'  onClick={() => handlebutton("/")}>/</button></div>        
         </div>
 
         <div className='column'>
-        <div><button id='botton4'>4</button></div>  
-        <div><button id='botton5'>5</button></div>        
-        <div><button id='botton6'>6</button></div>        
-        <div><button id='botton*'>*</button></div>        
+        <div><button id='botton4' className='button' onClick={() => handlebutton("4")}>4</button></div>  
+        <div><button id='botton5'  className='button' onClick={() => handlebutton("5")}>5</button></div>        
+        <div><button id='botton6' className='button' onClick={() => handlebutton("6")}>6</button></div>        
+        <div><button id='botton*' className='button' onClick={() => handlebutton("*")}>*</button></div>        
         </div>
 
         <div className='column'>
-        <div><button id='botton1'>1</button></div>  
-        <div><button id='botton2'>2</button></div>        
-        <div><button id='botton3'>3</button></div>        
-        <div><button id='botton-'>-</button></div>        
+        <div><button id='botton1' className='button' onClick={() => handlebutton("1")}>1</button></div>  
+        <div><button id='botton2'  className='button' onClick={() => handlebutton("2")}>2</button></div>        
+        <div><button id='botton3'  className='button' onClick={() => handlebutton("3")}>3</button></div>        
+        <div><button id='botton-'  className='button' onClick={() => handlebutton("-")}>-</button></div>        
         </div>
 
         <div className='column'>
